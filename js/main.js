3dd8ebf6f3ca2cde2a76882682748489b0d683aa
Dmitry Dadeco <dimdaxdadeco@mail.ru>
2016-11-13 16:12:16 +0300

    forum-main

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	$("#top").on("click", function (event) {
+			//отменяем стандартную обработку нажатия по ссылке
+			event.preventDefault();
+
+			//забираем идентификатор бока с атрибута href
+			var id  = $(this).attr('href'),
+
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+				top = $(id).offset().top;
+			
+			//анимируем переход на расстояние - top за 1500 мс
+			$('body,html').animate({scrollTop: top}, 800);
+		});
+	/* ------  */
+
+		var StickyElement = function(node){
+	  var doc = $(document), 
+	      fixed = false,
+	      anchor = node.find('.sticky-anchor'),
+	      content = node.find('menu');
+	  
+	  var onScroll = function(e){	
+	    var docTop = doc.scrollTop(),
+	        anchorTop = anchor.offset().top;
+	    
+	    if(docTop > anchorTop){
+	      if(!fixed){
+	      	$('menu .logo img').attr("src","img/logo-icon.png");
+	      	$('.search-icon').css('display','none');
+	        anchor.height(content.outerHeight());
+	        content.addClass('fixed');        
+	        fixed = true;
+	      }
+	    }  else   {
+	      if(fixed){
+	      	$('menu .logo img').attr("src","img/logo.png");
+	      	$('.search-icon').css('display','block');
+	        anchor.height(0);
+	        content.removeClass('fixed'); 
+	        fixed = false;
+	      }
+	    }
+	  };
+
+		  $(window).on('scroll', onScroll);
+		};
+
+		var fixMenu = new StickyElement($('#wrapper'));		
+});
